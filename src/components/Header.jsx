0988ff8a4451bdcb0a6405be0e7cb9a918eefda2
jyotendra singh chauhan <jyotendrachandan02@gmail.com>
2024-07-10 01:22:00 +0530

    Header Naming Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,8 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+          {/* <img src={brainwave} width={190} height={40} alt="Brainwave" /> */}
+          <h6>Jyotendra Chauhan</h6>
         </a>
 
         <nav
@@ -67,14 +68,14 @@ const Header = () => {
           <HamburgerMenu />
         </nav>
 
-        <a
+        {/* <a
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          New account
-        </a>
+          Contact
+        </a> */}
         <Button className="hidden lg:flex" href="#login">
-          Sign in
+        Contact
         </Button>
 
         <Button
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,6 @@ const Hero = () => {
 
   return (
     <Section
-    
       className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
@@ -24,9 +23,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Engineering the Future with&nbsp;
             <span className="inline-block relative">
-              Brainwave{" "}
+              Jyotendra{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -37,8 +36,9 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+            With a love for tech since childhood, I pursue depth in
+            understanding and excellence in solving real-world problems through
+            innovation.
           </p>
           <Button href="/pricing" white>
             Get started
